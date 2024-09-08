441acdea0c82d44bcb63367d3dc352c63f89afa7
Saad - DxD <131401635+Hyodu@users.noreply.github.com>
2024-09-08 15:44:21 +0100

    testcases

---
+++ testcases/compound-operators.js
@@ -0,0 +1,43 @@
+function handleCodeRun(code) {
+  try {
+    const capturedOutput = [];
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+      capturedOutput.push(
+        args.map((arg) => {
+          if (typeof arg === "object" && arg !== null) {
+            return JSON.stringify(arg);
+          }
+          return arg.toString();
+        }).join(" "),
+      );
+      originalConsoleLog(...args);
+    };
+    if (code) {
+      eval(code);
+    }
+    console.log = originalConsoleLog;
+    return capturedOutput.join("\n");
+  } catch (error) {
+    return `${error}`;
+  }
+}
+
+code += "\nconsole.log(myVar);";
+
+try {
+  const output = handleCodeRun(code);
+  console.log("output", output);
+  if (output !== "15") {
+    msg = "قيمة myVar غير صحيحة";
+  }
+} catch (error) {
+  msg = error;
+}
+
+const regex = /myVar\s*\+\=\s*10/;
+if (!regex.test(code)) {
+  msg = "لم يتم إضافة 10 إلى قيمة myVar";
+} else {
+  isPass = true;
+}
