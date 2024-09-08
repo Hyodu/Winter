417b33c98324e34c58c0f66a76f55652984bac65
Saad - DxD <131401635+Hyodu@users.noreply.github.com>
2024-09-08 15:42:15 +0100

    ❄️ - Website Bases

---
+++ static/sw.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = 'cache-v1';
+// const no_cache_urls = [
+//     "/sw-cache.json",
+// ]
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        () => {
+            const cacheNames = caches.keys();
+            Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+            cacheNewResources()
+        }
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(function () {
+            return cacheNewResources();
+        })
+    );
+});
+
+async function cacheNewResources() {
+    const resp = await fetch('/sw-cache.json');
+    const data = await resp.json();
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([...data, "/", "/offline", "/courses"]);
+}
+
+self.addEventListener('fetch', async function (event) {
+    // const url = new URL(event.request.url).pathname
+    // ! this code update cache on every request it is not good for performance, but it make app works offline
+    // TODO: remove this code block
+    if (event.request.method === 'GET' && navigator.onLine && event.request.url.startsWith('http')) {
+        try {
+            const cache = await caches.open(CACHE_NAME);
+            const resp = await fetch(event.request);
+            cache.put(event.request, resp.clone());
+            event.respondWith(resp);
+        } catch (error) {
+            // TODO: handle error
+            // console.error('Error fetching and caching:', error);
+        }
+    }
+    // ! this code block is for offline support temproray
+    // TODO: remove this code block
+    if (!navigator.onLine) {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else {
+                    return caches.match(new Request('/offline'));
+                }
+            })
+        );
+    }
+    // TODO: Implement cache strategy
+});
