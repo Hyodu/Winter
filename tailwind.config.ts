98008ab33a93b132784c31b6557c8abb16478beb
Saad - DxD <131401635+Hyodu@users.noreply.github.com>
2024-09-08 16:01:03 +0100

    ❄️ - Winter Theme

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import { type Config } from "tailwindcss";
+import daisyui from "daisyui";
+
+export default {
+  content: [
+    "{routes,islands,components}/**/*.{ts,tsx}",
+    "routes/**/*.{ts,tsx}",
+    "islands/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+  ],
+  plugins: [daisyui],
+  daisyui: {
+    log: false,
+    themes: ["dim", "nord"],
+  },
+} satisfies Config;
