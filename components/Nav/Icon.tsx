303c741c55fd135ac0ef7748b970ebf3411c5f40
Saad - DxD <131401635+Hyodu@users.noreply.github.com>
2024-09-08 14:46:33 +0100

    Winter icon

---
+++ components/Nav/Icon.tsx
@@ -0,0 +1,9 @@
+export default function Icon() {
+  return (
+    <div class="flex items-center">
+      <span class="text-blue-400">JS</span>
+      <span class="ml-2 font-bold text-2xl">الشتاء</span>
+    </div>
+  );
+}
+ 
