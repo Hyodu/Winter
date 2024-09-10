bb868f1516d4acacef397e0e21d867d03e90e32d
Saad - DxD <131401635+Hyodu@users.noreply.github.com>
2024-09-10 18:49:12 +0100

    testcase : intro

---
+++ testcases/functions/intro.js
@@ -0,0 +1,36 @@
+function handleCodeRun(code) {
+  try {
+    const capturedOutput = [];
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+      capturedOutput.push(
+        args.map((arg) => {
+          if (typeof arg === "object" && arg !== null) {
+            return JSON.stringify(arg);
+          }
+          return arg.toString();
+        }).join(" "),
+      );
+      originalConsoleLog(...args);
+    };
+    if (code) {
+      eval(code);
+    }
+    console.log = originalConsoleLog;
+    return capturedOutput.join("\n");
+  } catch (error) {
+    return `${error}`;
+  }
+}
+
+code += "\nconsole.log(area(5, 75));"
+const output = handleCodeRun(code);
+console.log(output);
+
+if (output) {
+  if (output.includes("375")) {
+    isPass = true;
+  } else {
+    msg = "هناك خطاء ما تأكد من شروط الاختبار"
+  }
+        }
